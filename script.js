1701f362f88226943eecfb8f29df68966640b227
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-03-09 19:02:25 -0300

    More Responsive Design and Starting add animations

---
+++ script.js
@@ -0,0 +1,11 @@
+var scrollbookimg = 
+
+window.addEventListener("load", function(event) {
+    document.querySelector(".book-exchange").style.left = "-1100px";
+    document.querySelector(".about-title").style.left = "-1100px";
+    document.querySelector(".about-text").style.left = "-1100px";
+});
+
+
+
+window.scrollBy(1000, 1000);
