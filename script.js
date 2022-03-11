198084dfa05550380da4a9fd3d529e35bb948455
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-03-10 23:15:06 -0300

    improving the animation

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-var scrollbookimg = 
-
 window.addEventListener("load", function(event) {
     document.querySelector(".book-exchange").style.left = "-1100px";
-    document.querySelector(".about-title").style.left = "-1100px";
-    document.querySelector(".about-text").style.left = "-1100px";
+    document.querySelector(".about-title").style.right = "1100px";
+    document.querySelector(".about-text").style.right = "1100px";
 });
 
 
+const showOnPx = 100;
+const aboutLocalization = document.querySelector(".about");
 
-window.scrollBy(1000, 1000);
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+    if(scrollContainer().scrollTop > 1800) {
+        console.log("Você chegou");
+        document.querySelector(".book-exchange").style.left = "0px";
+        document.querySelector(".about-title").style.right = "0px";
+        document.querySelector(".about-text").style.right = "0px";
+        
+    }
+})
+
