e3f05504ad315410bbafabdcf688d5d35bf53523
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-03-16 12:54:47 -0300

    footer

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const showOnPx = 100;
 const aboutLocalization = document.querySelector(".book-exchange");
+const inputButton = document.querySelector(".input-button");
+const mainBook = document.querySelector(".main-img");
+
 
 window.addEventListener("load", function(e) {
     document.querySelector(".book-exchange").style.left = "-1100px";
+    mainBook.style.top = "-600px";
+    if(window.innerWidth > 1480){
+    mainImgLoader();
+    }
 });
 
-
+function mainImgLoader(){
+    document.querySelector(".main-img").style.top = "unset";    
+}
 
 const scrollContainer = () => {
     return document.documentElement || document.body;
